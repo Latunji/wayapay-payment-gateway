efac40774a97844b6fd69eacdb19ee5f8c401d40
Adeshina <91054814+integral1@users.noreply.github.com>
2022-05-05 18:12:45 +0100

    updated codes

--- src/main/java/com/wayapaychat/paymentgateway/cron/CronService.java
+++ src/main/java/com/wayapaychat/paymentgateway/cron/CronService.java
@@ -49,7 +49,7 @@ public class CronService {
     private PaymemtGatewayEntityListener paymemtGatewayEntityListener;
 
 
-        @Scheduled(cron = "0 0 0 * * *")
+    @Scheduled(cron = "0 0 0 * * *")
     private void runEveryFiveSeconds() {
         updateTransactionStatus();
     }

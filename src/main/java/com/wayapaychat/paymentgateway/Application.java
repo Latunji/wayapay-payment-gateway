9ac0ac67562c23949c5ca081ce01e80b416cb9ef
Adeshina <91054814+integral1@users.noreply.github.com>
2022-05-09 14:30:59 +0100

    updated added kafka

--- src/main/java/com/wayapaychat/paymentgateway/Application.java
+++ src/main/java/com/wayapaychat/paymentgateway/Application.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.modelmapper.Conditions;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -45,4 +48,11 @@ public class Application {
         return objectMapper;
     }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setSkipNullEnabled(true).setPropertyCondition(Conditions.isNotNull())
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
+    }
 }

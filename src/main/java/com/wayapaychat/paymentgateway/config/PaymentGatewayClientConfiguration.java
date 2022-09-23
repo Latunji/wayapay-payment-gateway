8cdf5b32c9ce1e9725f07ea7d8f1c51cdd9b8ad2
404: CTO_not_found <ebukauche52@gmail.com>
2022-09-23 14:06:22 +0100

    stopped retrying +500 request infinitely

--- src/main/java/com/wayapaychat/paymentgateway/config/PaymentGatewayClientConfiguration.java
+++ src/main/java/com/wayapaychat/paymentgateway/config/PaymentGatewayClientConfiguration.java
@@ -25,15 +25,16 @@ public class PaymentGatewayClientConfiguration implements ErrorDecoder {
     public Exception decode(String methodKey, Response response) {
         FeignException exception = feign.FeignException.errorStatus(methodKey, response);
         int status = response.status();
-        if (status >= 500) {
-            log.error("-------||| RETRYING REQUEST ------|||| {0}", exception);
-            return new RetryableException(
-                    response.status(),
-                    exception.getMessage(),
-                    response.request().httpMethod(),
-                    exception, null,
-                    response.request());
-        }
+        // this code is irrelevant here; TODO: do this in a separate function and call it when needed
+//        if (status >= 500) {
+//            log.error("-------||| RETRYING REQUEST ------|||| {0}", exception);
+//            return new RetryableException(
+//                    response.status(),
+//                    exception.getMessage(),
+//                    response.request().httpMethod(),
+//                    exception, null,
+//                    response.request());
+//        }
         return exception;
     }
 }
